246cc3ef2bda9ef7d425d32caaa8892e7cae8182
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-02-12 01:24:18 +0700

    12 Feb 2022

---
+++ doWHILE.c
@@ -0,0 +1,21 @@
+/* File         : doWHILE.c */
+/* Penulis      : Fauzyah Hadirahma, email faubelajar@gmail.com */
+/* Deskripsi    : print "Saya senang" sebanyak 3 kali */
+
+#include <stdio.h>
+int main ()
+{
+    /* KAMUS */
+    int i=0;
+    
+    /* ALGORITMA */
+    do
+        {
+            printf ("Saya senang belajar bahasa C \n");
+            i++;
+        }
+        
+    while (i<5);
+    
+    return 0;
+}
